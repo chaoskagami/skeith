6d3b2333ce4b08feacd01d8f707e934d57d28709
chaoskagami <chaos.kagami@gmail.com>
2016-07-01 10:50:04 -0400

    Add build script to get nightly 'skeith' builds running via github

---
+++ checkbuild.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+FORCE=0
+
+if [ ! -d "corbenik" ]; then
+    git clone --recursive https://github.com/chaoskagami/corbenik corbenik
+    FORCE=1
+fi
+
+cd corbenik
+
+make clean
+
+# ==============================================
+
+git remote update
+git fetch
+
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+BASE=$(git merge-base @ @{u})
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+    if [ $FORCE = 0 ]; then
+        exit 0
+    fi
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    git pull
+else
+    echo "Diverged"
+    exit 1
+fi
+
+make fw_folder=skeith name=Skeith release
+
+mv rel ../rel
+
+REV=$(git rev-parse HEAD)
+
+cd ..
+
+git add rel
+
+git commit -m "Automated - build succeeded: $REV"
+
+git push
+
+exit 0
